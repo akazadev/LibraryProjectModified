a1096f00cb2fa8fb3ed900feda9cadcc34739a72
TheCode <codemma3@gmail.com>
2024-01-05 16:08:53 +0000

    Creation des differentes classes et des methodes. Il faut noter que les methodes ne sont pas encore toutes faites

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        sayHello();
-    }
-
-    public static void sayHello(){
-        System.out.println("HELLOWORLD");
-    }
+        }
 }
